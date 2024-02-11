113003112a20076f96856ba73745aa913000f317
Ladydee3 <delisiletwala62@gmail.com>
2024-02-11 11:47:56 -0800

    0 README.md and AUTHOURS

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Initializes the package"""
+from models.engine.filestorage import filestorage
+storage = Filestorage()
+storage.reload()
---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""""This module creates a Amenity class"""
+
+from model.base_model import BaseModel
+
+
+Class Amenity(BaseModel):
+    """Class for managing amenity objects"""
+    name = ""
